4f3cdb97dba30da15c23e90377d5b05775d253a3
hanghanghe <1844458215@qq.com>
2021-05-06 22:15:31 +0800

    20210506

--- MicroService/cloud-config-center-3344/src/main/java/hang/he/Config3344.java
+++ MicroService/cloud-config-center-3344/src/main/java/hang/he/Config3344.java
@@ -1,2 +1,13 @@
-package hang.he;public class Config3344 {
+package hang.he;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config3344.class,args);
+    }
 }
