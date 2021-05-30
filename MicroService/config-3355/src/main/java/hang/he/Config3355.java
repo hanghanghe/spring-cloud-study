fde17dd375ca40b41bf27a68804b29c5142d09af
hanghanghe <1844458215@qq.com>
2021-05-30 21:46:33 +0800

    cloud-study 20210530

---
+++ MicroService/config-3355/src/main/java/hang/he/Config3355.java
@@ -0,0 +1,16 @@
+package hang.he;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.swing.*;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Config3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config3355.class,args);
+    }
+}
